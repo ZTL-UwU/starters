9432184e0d5b40ecedbf811d8dd6d4ee91e99beb
ZTL-UwU <zhangtianli2006@163.com>
2024-10-19 19:38:35 +0800

    chore: update deps
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- minimal/nuxt.config.ts
+++ minimal/nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@vueuse/nuxt',
     '@nuxt/image',
     '@nuxt/icon',
   ],
+
   future: {
     compatibilityVersion: 4,
   },
+
+  compatibilityDate: '2024-10-03',
 });
